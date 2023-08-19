19ebbd4fd4c7a9ce1eeb0445a0ad202c4bb389bc
deepak-chaurasiya12 <dchaurasiya277@gmail.com>
2023-08-19 18:26:42 +0530

    Updates

---
+++ precache-manifest.e703804160d36ecd1bc3979001332beb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "750ea3ff8a6fee41980d",
+    "url": "/NewsApp/static/css/main.18c014e9.chunk.css"
+  },
+  {
+    "revision": "750ea3ff8a6fee41980d",
+    "url": "/NewsApp/static/js/main.3117835d.chunk.js"
+  },
+  {
+    "revision": "f4b1c72d76532c76e52f",
+    "url": "/NewsApp/static/js/runtime~main.883cbf20.js"
+  },
+  {
+    "revision": "1d361e2d5e95ffb5a3e9",
+    "url": "/NewsApp/static/js/2.c0be098f.chunk.js"
+  },
+  {
+    "revision": "279e4fca54e5d8e57f31",
+    "url": "/NewsApp/static/js/3.33a557c1.chunk.js"
+  },
+  {
+    "revision": "e5e9172fc240993bd699f593370b2a56",
+    "url": "/NewsApp/static/media/loading.e5e9172f.gif"
+  },
+  {
+    "revision": "3562571d047a0c9a34722b468e655fba",
+    "url": "/NewsApp/static/media/pic.3562571d.webp"
+  },
+  {
+    "revision": "1b8fc17a4e8e8f3f7d5071b881021a79",
+    "url": "/NewsApp/index.html"
+  }
+];
